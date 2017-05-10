907908259b77e3711ff07275197f18c004718b83
sysleg <zlou.sysleg@gmail.com>
2017-05-10 21:29:26 +0300

    реализована простая анимация заката по клику

--- app/src/main/java/com/bignerdranch/android/sunset/SunsetActivity.java
+++ app/src/main/java/com/bignerdranch/android/sunset/SunsetActivity.java
@@ -1,13 +1,13 @@
 package com.bignerdranch.android.sunset;
 
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class SunsetActivity extends AppCompatActivity {
+public class SunsetActivity extends SingleFragmentActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sunset);
+    protected Fragment createFragment() {
+        return SunsetFragment.newInstance();
     }
 }
---
+++ app/src/main/java/com/bignerdranch/android/sunset/SunsetFragment.java
@@ -0,0 +1,48 @@
+package com.bignerdranch.android.sunset;
+
+import android.animation.ObjectAnimator;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Dmitry on 10.05.2017.
+ */
+
+public class SunsetFragment extends Fragment {
+    private View mSceneView;
+    private View mSunView;
+    private View mSkyView;
+
+    public static SunsetFragment newInstance(){
+        return new SunsetFragment();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_sunset,container,false);
+        mSceneView = view;
+        mSunView = view.findViewById(R.id.sun);
+        mSkyView = view.findViewById(R.id.sky);
+
+        mSceneView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startAnimation();
+            }
+        });
+        return view;
+    }
+
+    private void startAnimation(){
+        float sunYStart = mSunView.getTop();
+        float sunYEnd = mSkyView.getHeight();
+
+        ObjectAnimator heightAnimator = ObjectAnimator.ofFloat(mSunView, "y", sunYStart, sunYEnd).setDuration(3000);
+        heightAnimator.start();
+    }
+}
